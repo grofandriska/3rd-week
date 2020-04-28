92564b9aa715fc634bf45df5c9e17bb1cb21307f
grofandriska <andrasgrof93@gmail.com>
2020-04-28 18:30:23 +0200

    2nd day's objects
    New objects created

--- WEB/java.js
+++ WEB/java.js
@@ -4,8 +4,36 @@ let data = {
     description : "Famous german tank destroyer from the 2ndWW area"
 }
 
-$('#kep').attr('src', data.photo);
 
-$('#photo-title').text( data.title);
 
-$('#photo-description').text( data.description);
+let pantherdata = {
+    photo:'Images/55a211e5f0329db4a140141aa1391ff1.jpg',
+    title : "Panther",
+    description :"Famous german medium/heavy tank"
+}
+
+let mausdata = {
+    photo:'Images/maus1.jpg',
+    title:"MAUS",
+    description:"the world's heaviest tank ever dreamed"
+
+
+}
+
+let panzer4data ={
+    photo:'Images/Panzer-IV-Ausf-G-3.jpg',
+    title:"Panzer IV G",
+    description:"Most produced german medium tank",
+}
+
+let tigerdata ={
+    photo:'Images/TigerITunis.jpg',
+    title:"Tiger ",
+    description:"World's most feared famous heavy tank",
+}
+
+$('#kep').attr('src', mausdata.photo);
+
+$('#photo-title').text( mausdata.title);
+
+$('#photo-description').text( mausdata.description);
