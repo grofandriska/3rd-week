4c626fd711baafce9e4db654b25b3132651c6dec
grofandriska <andrasgrof93@gmail.com>
2020-04-29 14:48:45 +0200

    Web
    3rd day almost done

--- WEB/java.js
+++ WEB/java.js
@@ -3,15 +3,11 @@ let data = {
     title : "Jagdpanzer 4",
     description : "Famous german tank destroyer from the 2ndWW area"
 }
-
-
-
 let pantherdata = {
     photo:'Images/55a211e5f0329db4a140141aa1391ff1.jpg',
     title : "Panther",
     description :"Famous german medium/heavy tank"
 }
-
 let mausdata = {
     photo:'Images/maus1.jpg',
     title:"MAUS",
@@ -19,21 +15,56 @@ let mausdata = {
 
 
 }
-
 let panzer4data ={
     photo:'Images/Panzer-IV-Ausf-G-3.jpg',
     title:"Panzer IV G",
     description:"Most produced german medium tank",
 }
-
 let tigerdata ={
     photo:'Images/TigerITunis.jpg',
     title:"Tiger ",
     description:"World's most feared famous heavy tank",
 }
 
-let currentPhoto = 1;
-let imagesData = [data,pantherdata];
-$('#kep').attr('src', imagesData[currentPhoto].photo);
-$('#photo-title').text( imagesData[currentPhoto].title);
-$('#photo-description').text(imagesData[currentPhoto].description);
+
+
+
+
+
+
+let currentPhoto = 0;
+let imagesData = [data,pantherdata,mausdata,panzer4data,tigerdata];
+
+let loadPhoto = (photoNumber) => {
+    $('#kep').attr('src', imagesData[photoNumber].photo); }
+ loadPhoto(currentPhoto)
+
+    
+    
+let loadtitle = (photoNumber) => {
+        $('#photo-title').text(imagesData[photoNumber].title)}
+loadtitle(currentPhoto)
+
+    
+    
+let loaddescription = (photoNumber) => {
+        $('#photo-description').text(imagesData[photoNumber].description)}
+loaddescription(currentPhoto)
+
+$('#jobb').click(() => {
+    currentPhoto++;
+    loadPhoto(currentPhoto);
+    if(currentPhoto < 4) { 
+        currentPhoto++; 
+      }
+  })
+
+  $('#bal').click(() => {
+    currentPhoto--;
+    loadPhoto(currentPhoto);
+    if(currentPhoto > 1) {
+        currentPhoto--;  
+      }
+  })
+
+  
